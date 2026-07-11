79f7986c1f4f9c3729be717552cfe3a5e0a8653d
simongerson <simongersonuk@gmail.com>
2026-07-11 13:27:01 +0300

    fix: mpsea stk in sessions 8

--- app/api/sessions/[id]/route.js
+++ app/api/sessions/[id]/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 
 
 import { query } from '@/lib/db'
-import { NextResponse } from 'next/server'
+
 
 export async function GET(request, { params }) {
   try {
